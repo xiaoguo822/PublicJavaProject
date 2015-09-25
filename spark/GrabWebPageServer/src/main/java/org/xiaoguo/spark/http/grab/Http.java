3ba9e4e441827811a649274bed487d0ed38f5108
xiaoguo822 <i@xiaoguo822.com>
2015-09-25 18:56:59 +0800

    添加了注释

--- spark/GrabWebPageServer/src/main/java/org/xiaoguo/spark/http/grab/Http.java
+++ spark/GrabWebPageServer/src/main/java/org/xiaoguo/spark/http/grab/Http.java
@@ -1,5 +1,11 @@
 package org.xiaoguo.spark.http.grab;
-
+/**
+ * 每一个url是一个单独的对象
+ * 请求的结果基本类型是byte[]
+ * 我们可以根据Content-Type决定返回的具体类型和后续操作
+ * @author xiaoguo
+ *
+ */
 public class Http {
 	private String url;
 	private HttpMethod method;
